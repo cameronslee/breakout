71c7b9b53f03da7bb8616b9af8fec0b8d05f62c5
Cameron Lee <cameronslee@hotmail.com>
2023-11-14 19:11:27 -0800

    start state and player movement

--- src/source.c
+++ src/source.c
@@ -7,10 +7,30 @@
 //----------------------------------------------------------------------------------
 // Defines
 //----------------------------------------------------------------------------------
-#define SQUARE_SIZE 32
-#define ROWS 16
-#define COLS 16
-#define MAX_BOMBS 20
+#define PLAYER_HEIGHT 16
+#define PLAYER_WIDTH 48
+#define ROWS_BRICKS 4
+#define COLS_BRICKS 7
+
+//------------------------------------------------------------------------------------
+// Types n Structs 
+//------------------------------------------------------------------------------------
+typedef struct Player {
+    Vector2 position;
+    Vector2 size;
+    float speed;
+} Player;
+
+typedef struct Ball {
+    Vector2 position;
+    Vector2 speed;
+    int radius;
+} Ball;
+
+typedef struct Brick {
+    Vector2 position;
+    bool active;
+} Brick;  
 
 //------------------------------------------------------------------------------------
 // Global Variables Declaration
@@ -19,17 +39,17 @@
 static const int screenWidth = 512;
 static const int screenHeight = 512;
 
-int board[ROWS][COLS]; //game board
-int bombs[ROWS][COLS]; //bomb locations
-int counts[ROWS][COLS]; //num surrounding mines
+static unsigned int score = 0;
 
 static int framesCounter = 0;
 static bool gameOver = false;
 static bool pause = false;
 static bool win = false;
 
-static Vector2 offset = { 0 };
-
+static Brick bricks[ROWS_BRICKS][COLS_BRICKS] = { 0 };
+static Vector2 brickSize = { 0 };
+Player player;
+Ball ball;
 //------------------------------------------------------------------------------------
 // Module Functions Declaration (local)
 //------------------------------------------------------------------------------------
@@ -82,11 +102,37 @@ bool IsWinner()
 
 void InitGame(void)
 {
+    brickSize = (Vector2){ 64, 32};
+
     srand(time(NULL));
     framesCounter = 0;
     gameOver = false;
     pause = false;
     win = false;
+    score = 0;
+
+    // Init Player
+    player.size.x = PLAYER_WIDTH;
+    player.size.y = PLAYER_HEIGHT;
+    player.position.x = screenWidth / 2;
+    player.position.y = screenHeight - player.size.x;
+    player.speed = 10.0;
+
+    // Init Ball
+    ball.position.x = screenWidth / 2;
+    ball.position.y = screenHeight / 2;
+    ball.speed.x = 5.0;
+    ball.speed.y = 5.0;
+    ball.radius = 8;
+
+    // Init Bricks
+    for (int i = 0; i < ROWS_BRICKS; i++) {
+      for (int j = 0; j < COLS_BRICKS; j++) {
+        bricks[i][j].position = (Vector2){ j*brickSize.x + 32 , i*brickSize.y + 8};
+        bricks[i][j].active = true;
+      }
+    }
+
 }
 
 void UpdateGame(void)
@@ -98,9 +144,14 @@ void UpdateGame(void)
             win = true;
         } 
         if (IsKeyPressed('P')) {
-            printf("Pause toggle\n");
-            pause = !pause;
+          pause = !pause;
         }
+
+        if (IsKeyDown('A')) player.position.x -= player.speed;
+        if ((player.position.x - player.size.x/2) <= 0) player.position.x = player.size.x/2;
+
+        if (IsKeyDown('D')) player.position.x += player.speed;
+        if ((player.position.x + player.size.x/2) >= screenWidth) player.position.x = screenWidth - player.size.x/2;
     }
     else
     {
@@ -115,6 +166,24 @@ void DrawGame(void)
 
         ClearBackground(RAYWHITE);
 
+        // Draw Player
+        DrawRectangle(player.position.x, player.position.y, player.size.x, player.size.y, DARKGREEN);
+
+        //Draw Score
+        char score_buff[4];
+        sprintf(score_buff, "%d", score);
+        DrawText(score_buff, 10, screenHeight - 20, 20, DARKBLUE);
+
+        // Draw Ball
+        DrawCircle(ball.position.x, ball.position.y, ball.radius, RED);
+        
+        // Draw Bricks
+        for (int i = 0; i < ROWS_BRICKS; i++) {
+          for (int j = 0; j < COLS_BRICKS; j++) {
+            if (bricks[i][j].active) DrawRectangle(bricks[i][j].position.x, bricks[i][j].position.y, brickSize.x-8, brickSize.y-8, GRAY);
+          }
+        }
+
         if (!gameOver)
         {
 
@@ -128,6 +197,7 @@ void DrawGame(void)
                 DrawText("WINNER! PRESS [ENTER] TO PLAY AGAIN", GetScreenWidth()/2 - MeasureText("WINNER! PRESS [ENTER] TO PLAY AGAIN", 20)/2, GetScreenHeight()/2 - 50, 20, DARKGREEN);
             }
             else
+
             {
                 DrawText("GAME OVER! PRESS [ENTER] TO PLAY AGAIN", GetScreenWidth()/2 - MeasureText("GAME OVER! PRESS [ENTER] TO PLAY AGAIN", 20)/2, GetScreenHeight()/2 - 50, 20, DARKGREEN);
             }
